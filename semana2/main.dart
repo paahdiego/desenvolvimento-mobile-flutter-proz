3d93deed3b0214c8839e48cbc9ecc978caa8c1ea
Patrick Diego <paah-diego@hotmail.com>
2022-08-04 00:32:11 -0300

    first commit

---
+++ semana2/main.dart
@@ -0,0 +1,13 @@
+void main() {
+  dynamic variavel = 5;
+
+  print('Primeiro tipo: ${variavel.runtimeType}');
+
+  //Primeiro tipo: int
+
+  variavel = 'texto';
+
+  print('Segundo tipo: ${variavel.runtimeType}');
+
+  //Segundo tipo: String
+}
