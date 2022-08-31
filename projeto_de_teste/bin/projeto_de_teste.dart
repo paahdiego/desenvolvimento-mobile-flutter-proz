d04dce7b42b1c32b0fe6f2e46534d59437d72967
paahdiego <paah-diego@hotmail.com>
2022-08-30 21:23:10 -0300

    adicionando novo projeto no repo

---
+++ projeto_de_teste/bin/projeto_de_teste.dart
@@ -0,0 +1,5 @@
+import 'package:projeto_de_teste/projeto_de_teste.dart' as projeto_de_teste;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projeto_de_teste.calculate()}!');
+}
---
+++ projeto_de_teste/test/projeto_de_teste_test.dart
@@ -0,0 +1,8 @@
+import 'package:projeto_de_teste/projeto_de_teste.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
